bd75ca5a15cc8e7aab7d8dd79c252a0b6df83353
Andrew Watkins <andrew@groat.nz>
2019-06-24 15:28:32 +1200

    add package_create example

---
+++ package_create.r
@@ -0,0 +1,20 @@
+source(file='ckan.r')
+
+package = package_create(
+  name="lure-dispenser-comparison-trial-from-r",
+  title= "R Lure Dispenser comparison trial, thrips, Australia, Perth",
+  notes= "Assess three thrips Lure delivery mechanisms:\n\n* P Paint pen\n* D Deer wick\n* C Control (no wick)",
+  private= FALSE,
+  owner_org= "plant-and-food-research-nz",
+  state= "active",
+  project_code= "P/1234",
+  author= "Mette Nielson",
+  project_leader_email=	"Mette.Nielson@plantandfood.co.nz",
+  data_custodian= "Mette Nielson",
+  data_custodian_email= "Mette.Nielson@plantandfood.co.nz",
+  other_researcher=	"David Teulon",
+  biometrician= "Ruth Butler",
+  credits= "Mel Walker",
+  license_id= "PFR Internal Use only",
+
+  as = "list")
